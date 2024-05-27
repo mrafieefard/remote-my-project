03b3304411390135b22dc5cc95038b58861d9c94
Mohammad Rafieefard <mrafieefard@gmail.com>
2024-05-27 23:44:20 +0330

    Add : read database url in env

--- api/db/base.py
+++ api/db/base.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
-engine = create_engine("sqlite:///touch.db",connect_args={"check_same_thread": False})
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    engine = create_engine(DATABASE_URL)
+else :
+    engine = create_engine("sqlite:///rmp.db",connect_args={"check_same_thread": False})
 conn = engine.connect()
 
 session = Session(bind=engine)
