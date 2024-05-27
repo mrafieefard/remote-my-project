d5c9f7d2b4d22ae082d302a606489c6139558b6f
Mohammad Rafieefard <mrafieefard@gmail.com>
2024-05-28 00:20:48 +0330

    chore : add psycopg2 to requirements

--- api/db/base.py
+++ api/db/base.py
@@ -8,6 +8,7 @@ if DATABASE_URL:
     engine = create_engine(DATABASE_URL)
 else :
     engine = create_engine("sqlite:///rmp.db",connect_args={"check_same_thread": False})
+
 conn = engine.connect()
 
 session = Session(bind=engine)
