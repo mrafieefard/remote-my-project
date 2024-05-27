39a24db9cc2f9b63043e5caf0a70f122acb32cdf
Mohammad Rafieefard <mrafieefard@gmail.com>
2024-05-28 01:20:07 +0330

    fix : change database url

--- api/db/base.py
+++ api/db/base.py
@@ -1,9 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-DATABASE_URL = "postgresql://test:test@localhost/rmpdb"
+DATABASE_URL = "postgresql://postgres:postgres@rmp-database/rmpdb"
+
 engine = create_engine(DATABASE_URL)
 
 session = Session(bind=engine)
